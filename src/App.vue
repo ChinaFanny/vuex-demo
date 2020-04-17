f11fe823449b31cf209c8c93aeafcd3d6321c25d
yangfan <yangfan@funguide.com.cn>
2020-04-17 15:43:32 +0800

    cout++ demo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>count:{{ count }}</h1>
+    <button @click="increase">点我自增1</button>
+    <button @click="changeValue">点我变为20</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increase() {
+      this.$store.commit("increase");
+    },
+    changeValue() {
+      this.$store.commit("changeValue", 20);
+    }
   }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state:{
+    count:0
+  },
+  mutations:{
+    increase(state) {
+      state.count++
+    },
+    changeValue(state, c) {
+      state.count = c
+    }
+  }
+})
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
